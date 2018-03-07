7c6075f0554bea3fd5c5ded6c014bc714c370133
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-03-07 08:38:32 -0500

    fix

--- variables.tf
+++ variables.tf
@@ -2,7 +2,9 @@
 provider "openstack" {
   user_name   = "admin"
   tenant_name = "admin"
-  password    = "f48beb215c5f1157f07894cf32e684983d84ee281366878d25e3e6e5c68a11"
-  auth_url    = "http://myauthurl:5000/v2.0"
+  domain_name = "default"
+  password    = "<your password>"
+  auth_url    = "http://<your public auth ip>:5000/v3"
   region      = "RegionOne"
+  insecure    = "true"
 }
