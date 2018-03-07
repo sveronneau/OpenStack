dc99247732be61b3992d4189440368fee1eea96e
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2018-03-07 08:51:24 -0500

    fix

--- basic_compute.tf
+++ basic_compute.tf
@@ -1,10 +1,11 @@
-# Basic instance with 1 network
+# Basic instance with 1 network and a Floating IP
 resource "openstack_compute_instance_v2" "basic" {
   name            = "basic"
-  image_id        = "cirros"
-  flavor_id       = "tempest_1"
+  image_name      = "cirros-gold"
+  flavor_name     = "tempest1"
   key_pair        = ""
   security_groups = ["default"]
+  user_data       = "#cloud-config\nhostname: coco_1.mydomain.com\nfqdn: coco_1.mydomain.com"
 
   metadata {
     this = "that"
@@ -13,4 +14,13 @@ resource "openstack_compute_instance_v2" "basic" {
   network {
     name = "private"
   }
+}
+
+resource "openstack_networking_floatingip_v2" "fip_1" {
+  pool = "public"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "fip_1" {
+  floating_ip = "${openstack_networking_floatingip_v2.fip_1.address}"
+  instance_id = "${openstack_compute_instance_v2.basic.id}"
 }
